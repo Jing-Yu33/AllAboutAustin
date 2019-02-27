c786b8c510c93a95aff2a204478d296764a282bc
Yixing Wang <edith.w0807@gmail.com>
2019-02-27 17:02:08 -0600

    Added survey slider (store selected value in state)

--- Frontend/src/components/survey/SliderBar.js
+++ Frontend/src/components/survey/SliderBar.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import './SliderBar.css'
 
 class SliderBar extends Component {
+    state = {
+        value: 50
+    }
 
     render() {
         return(
-            <form action="" method="GET">
-                <div className="form-group">
-                    <label for="formControlRange">Weight</label>
-                    <input type="range" className="form-control-range" id="formControlRange" />
+                <div className="silder form-group">
+                    <label htmlFor="formControlRange" className="text-center"> {this.props.category} </label>
+                    <input 
+                        type="range" className="form-control-range" id="formControlRange" 
+                        value={this.state.value}
+                        onChange={(e)=>this.setState({value: e.target.value})}
+                        onBlur ={(e)=>{this.props.onSliderBarFocusOut([this.props.category], this.state.value)}}        
+                    />
                 </div>
-            </form>
         )
     }
 }
--- Frontend/src/components/survey/Survey.js
+++ Frontend/src/components/survey/Survey.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-
 import TopBar from "../navbar/TopBar";
+import Carousel from './Carousel';
 import SliderBar from './SliderBar'
+
+import './Survey.css'
 class Survey extends Component {
 
+    state = {
+            Food: 50,
+            Traffic: 50,
+            Education: 50
+    }
+
+    onSliderBarFocusOut = (category, value) => {
+        console.log(category+ " "+ value);
+        this.setState({
+                [category]: value
+        })
+    }
+
     render() {
+        console.log(this.state);
         return(
             <div>
                 <TopBar className="topBar"/>
                 <div className="container">
-                    <h1>Survey Page</h1>
-                    <SliderBar />
+                    <form id="survey-form" action="" method="GET">
+                        <SliderBar category="Food" onSliderBarFocusOut={this.onSliderBarFocusOut}/>
+                        <SliderBar category="Education" onSliderBarFocusOut={this.onSliderBarFocusOut}/>
+                        <SliderBar category="Traffic" onSliderBarFocusOut={this.onSliderBarFocusOut}/>
+                        <button type="button" className="btn btn-outline-success btn-lg btn-block">Submit</button>
+                        <p>Selected Value stored in this.state</p>
+                        <p>Food: {this.state.Food}, Education: {this.state.Education}, Traffic: {this.state.Traffic}</p>
+                    </form>
                 </div>
             </div>
         )
