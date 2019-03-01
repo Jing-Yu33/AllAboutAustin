ea0e336b1276e791f4c20f24bafc134e32ad1cd5
Yixing Wang <edith.w0807@gmail.com>
2019-03-01 16:22:06 -0600

    Added Responsive to About page

--- DataCollection/src/main/java/database/datacollection/DataCollectionMain.java
+++ DataCollection/src/main/java/database/datacollection/DataCollectionMain.java
@@ -11,7 +11,7 @@ public class DataCollectionMain {
 	public static void main(String[] args) {
 		//demoSodaCollector();
 		//demoZomatoCollector();
-		demoSchoolCollector();
+		//demoSchoolCollector();
 	}
 
 	private static void demoSodaCollector() {
