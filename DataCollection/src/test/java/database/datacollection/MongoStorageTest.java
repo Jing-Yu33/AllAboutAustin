ce43d21988dd500a0456b076dc17e00e2bdfd5dc
Graysless <graysonwatkins@gmail.com>
2019-03-29 17:53:53 -0500

    cleaned up MongoStorage, and created a junit test class for MongoStorage

---
+++ DataCollection/src/test/java/database/datacollection/MongoStorageTest.java
@@ -0,0 +1,37 @@
+package database.datacollection;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class  MongoStorageTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MongoStorageTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MongoStorageTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void test()
+    {
+        assertTrue( true );
+    }
+}
+
