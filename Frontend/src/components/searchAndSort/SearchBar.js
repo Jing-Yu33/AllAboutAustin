eb20e0fab017c1a486767b03411853da31e72a10
Yixing Wang <edith.w0807@gmail.com>
2019-04-09 12:50:05 -0500

    minor changes on styling

--- Frontend/src/components/searchAndSort/SearchBar.js
+++ Frontend/src/components/searchAndSort/SearchBar.js
@@ -12,14 +12,14 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <form >
+            <form>
                 <div className="input-group mb-2">
                     <label htmlFor="search"></label>
                     <input type="text" className="form-control" aria-describedby="search" placeholder="Search By Zipcode" 
                         name="term" value = {this.state.value} onChange = {(e) => this.setState({value: e.target.value})}
                     />
                     <div className="input-group-append">
-                        <button onClick={(e) => this.onSearchBarSubmit(e)}className="btn btn-outline-primary" type="button"><i className="fas fa-search"></i></button>
+                        <button onClick={(e) => this.onSearchBarSubmit(e)} style={{zIndex: '0'}} className="btn btn-outline-primary" type="button"><i className="fas fa-search"></i></button>
                     </div>
                 </div>
             </form>
