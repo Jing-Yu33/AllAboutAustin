dcdcd3980d546769d2914c40a4b2ea00b2197c45
Zach Wempe <zdwempe@gmail.com>
2019-04-18 12:34:01 -0500

    more testing updates

--- Frontend/test/chromeTest.py
+++ Frontend/test/chromeTest.py
@@ -8,21 +8,32 @@ def main():
 
 def homePageUI(driver):
     assert driver.title == "All About Austin" , "Title incorrect!"
-
     verifySurvey(driver)
+    driver.get('http://localhost:3000')
+    verifyHomePageButton(driver)
 
 def verifySurvey(driver):
     """
     This method checks the survey page to ensure that there are 3 sliders
     and that each slider has the correct default value of 50
     """
-    links = driver.find_elements_by_tag_name('a')
     btn = driver.find_element_by_link_text('Start')
     btn.click()
+    url = driver.current_url
+    assert url == 'http://localhost:3000/survey' , 'Survey URL incorrect!'
     els = driver.find_elements_by_class_name('form-control-range')
     assert len(els) == 3 , 'Number of sliders incorrect!'
     for i in els:
-        assert i.get_attribute('value') == "50" , "Default Values incorrect!"
+        assert i.get_attribute('value') == '50' , 'Default Values incorrect!'
+
+def verifyHomePageButton(driver):
+    btn = driver.find_element_by_partial_link_text('AAA')
+    btn.click()
+    url = driver.current_url
+    assert url == 'http://localhost:3000/' , 'HomePage URL incorrect!'
+
+
+
 
 if __name__ == "__main__":
     main()
--- Frontend/test/testEntry.py
+++ Frontend/test/testEntry.py
@@ -1,15 +1,11 @@
 import chromeTest
-import firefoxTest
-import operaTest
-import ieTest
-import safariTest
 
 def main():
     chromeTest.main()
-    firefoxTest.main()
-    operaTest.main()
-    ieTest.main()
-    safariTest.main()
+    #firefoxTest.main()
+    #operaTest.main()
+    #ieTest.main()
+    #safariTest.main()
 
 if __name__ == "__main__":
     main()
