4875d85a4265c46f946b25e8f91fdc55ac319502
Yixing Wang <edith.w0807@gmail.com>
2019-04-19 14:33:57 -0500

    add a switch control for recommendation

--- Frontend/src/components/navbar/BasicControls.js
+++ Frontend/src/components/navbar/BasicControls.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import GoogleAuth from './GoogleAuth';
 
 class BasicControls extends Component {
 
     render() {
+            const recomUrl = `/recommendation?food=${this.props.weight.Food}&traffic=${this.props.weight.Traffic}&education=${this.props.weight.Education}`;
             return(
                 <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to={recomUrl}>Recommendation</Link>
+                    </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/about">About</Link>
                     </li>
@@ -17,5 +22,11 @@ class BasicControls extends Component {
             )
     }
 }
-export default BasicControls;
+
+const mapStateToProps = (state) => {
+    return {
+        weight: state.weight
+    }
+}
+export default connect(mapStateToProps)(BasicControls);
 
