94a03e785711a5eaa193881726db6cede978d259
cpe342 <canyon@utexas.edu>
2019-04-19 10:40:40 -0500

    Styling for map legend and update to 10 total colors in map itself

--- Frontend/src/components/map/heatmap/HeatMap.js
+++ Frontend/src/components/map/heatmap/HeatMap.js
@@ -72,20 +72,22 @@ class HeatMap extends Component {
                 <div className="row">
                 <div className="col-5">
                 <ul className="list-unstyled">
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;10%</li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;20% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;30% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;40% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;50% </li>
+                  
+                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png" alt=""/>&nbsp;10%</li>
+                  <li><img src="https://i.imgur.com/lSMtmKA.png" alt=""/>&nbsp;20%</li>
+                  <li><img src="https://i.imgur.com/xx72TDD.png" alt=""/>&nbsp;30% </li>
+                  <li><img src="https://i.imgur.com/sIlIoNX.png" alt=""/>&nbsp;40% </li>
+                  <li><img src="https://i.imgur.com/Wrac0Mc.png" alt=""/>&nbsp;50% </li>
+                   
                 </ul>
                 </div>
                 <div className="col-5">
                 <ul className="list-unstyled">
-                   <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;60% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;70% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;80% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;90% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;100%</li>
+                  <li><img src="https://i.imgur.com/LpUYGQO.png" alt=""/>&nbsp;60% </li>
+                  <li><img src="https://i.imgur.com/pY6qMwT.png" alt=""/>&nbsp;70% </li>
+                  <li><img src="https://i.ibb.co/Sm5c0mG/col9.png" alt=""/>&nbsp;80% </li>
+                  <li><img src="https://i.imgur.com/uFyOFkC.png" alt=""/>&nbsp;90%</li>
+                  <li><img src="https://i.ibb.co/txKQTK1/col11.png" alt=""/>&nbsp;100%</li>
                 </ul>
                 </div>
                 </div>
@@ -108,20 +110,22 @@ class HeatMap extends Component {
                 <div className="row">
                 <div className="col-5">
                 <ul className="list-unstyled">
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;10%</li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;20% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;30% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;40% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;50% </li>
+                  
+                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png" alt=""/>&nbsp;10%</li>
+                  <li><img src="https://i.imgur.com/lSMtmKA.png" alt=""/>&nbsp;20%</li>
+                  <li><img src="https://i.imgur.com/xx72TDD.png" alt=""/>&nbsp;30% </li>
+                  <li><img src="https://i.imgur.com/sIlIoNX.png" alt=""/>&nbsp;40% </li>
+                  <li><img src="https://i.imgur.com/Wrac0Mc.png" alt=""/>&nbsp;50% </li>
+                   
                 </ul>
                 </div>
                 <div className="col-5">
                 <ul className="list-unstyled">
-                   <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;60% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;70% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;80% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;90% </li>
-                  <li><img src="https://i.ibb.co/WHH8fHc/3288bd.png"/>&nbsp;100%</li>
+                  <li><img src="https://i.imgur.com/LpUYGQO.png" alt=""/>&nbsp;60% </li>
+                  <li><img src="https://i.imgur.com/pY6qMwT.png" alt=""/>&nbsp;70% </li>
+                  <li><img src="https://i.ibb.co/Sm5c0mG/col9.png" alt=""/>&nbsp;80% </li>
+                  <li><img src="https://i.imgur.com/uFyOFkC.png" alt=""/>&nbsp;90%</li>
+                  <li><img src="https://i.ibb.co/txKQTK1/col11.png" alt=""/>&nbsp;100%</li>
                 </ul>
                 </div>
                 </div>
