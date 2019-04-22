1b612444dc3bb18b4b3ebead9ff1399b52ad34ba
Zach Wempe <zdwempe@gmail.com>
2019-04-21 23:36:32 -0500

    slightly refactored frontend testing and added functionality

--- Frontend/test/testEntry.py
+++ Frontend/test/testEntry.py
@@ -1,11 +1,19 @@
-import chromeTest
+from selenium import webdriver
+import browserTest
 
 def main():
-    chromeTest.main()
-    #firefoxTest.main()
-    #operaTest.main()
-    #ieTest.main()
-    #safariTest.main()
+    driver = webdriver.Chrome(executable_path = "./chromedriver")
+    browserTest.test(driver)
+    driver = webdriver.Firefox(executable_path = "./geckodriver")
+    browserTest.test(driver)
+    #options = webdriver.ChromeOptions()
+    #options.binary_location = "./operadriver"
+    #driver = webdriver.Opera(options=options, executable_path = "./operadriver")
+    #browserTest.test(driver)
+    #driver = webdriver.Ie()
+    #browserTest.test(Ie)
+    #driver = webdriver.Safari()
+    #browserTeset.test(driver)
 
 if __name__ == "__main__":
     main()
